f4162f9b9ae124eb6fca6792871df6bc56c62920
farrukh90 <farrukhsadykov@gmail.com>
2022-01-19 23:41:00 +0000

    added readme

--- EKS/EKS/main.tf
+++ EKS/EKS/main.tf
@@ -78,6 +78,7 @@ module "my-cluster" {
         instance_type = var.instance_type
         asg_max_size = var.asg_max_size
         asg_min_size = var.asg_min_size #min size should be 1
+        image = "ami-05674d6c91201b508"
                 }
         ]
         tags = var.tags
